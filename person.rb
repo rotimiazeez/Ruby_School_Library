18e871802206e1648686c76de57706a53cf1110f
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-11-11 11:28:08 -0800

    fix: fix person.rb

--- person.rb
+++ person.rb
@@ -1,33 +1,31 @@
+
 require 'corrector'
 require 'rental'
+
 
 class Person
   attr_accessor :name, :age, :rentals
   attr_reader :id
-
-  def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
-    @name = name
+  @@ -11,11 +13,13 @@ def initialize(age:, name: 'Unknown', parent_permission: true)
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
+
     @rentals = []
   end
 
   def add_rental(date, book)
     Rental.new(date, self, book)
+
   end
 
   def can_use_services?
     of_age? || @parent_permission
   end
-
   def validate_name
     @name = @corrector.correct_name(@name)
   end
-
   private
-
   def of_age?
     @age >= 18
   end
