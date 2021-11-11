38601010c154c01174b31d5ee40249bee731e832
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-11-11 13:16:39 -0800

    fix: fix linter

--- person.rb
+++ person.rb
@@ -4,7 +4,8 @@ require 'rental'
 class Person
   attr_accessor :name, :age, :rentals
   attr_reader :id
-  @@ -11,11 +13,13 @@ def initialize(age:, name: 'Unknown', parent_permission: true)
+
+  def initialize(age, _name = 'Unknown', parent_permission: true)
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
@@ -19,12 +20,13 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
-  
+
   def validate_name
     @name = @corrector.correct_name(@name)
   end
-  
+
   private
+
   def of_age?
     @age >= 18
   end
