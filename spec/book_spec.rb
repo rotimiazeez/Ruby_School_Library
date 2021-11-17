15880b572d900bf42118005ca84582088a6dc315
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-11-17 13:35:29 -0600

    set enviroment for testing

---
+++ spec/book_spec.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+
+describe 'Book' do
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+  context 'when condition' do
+    it 'succeeds' do
+      skip 'Not implemented'
+    end
+  end
+end
---
+++ spec/student_spec.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+
+describe 'Student' do
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+  context 'when condition' do
+    it 'succeeds' do
+      skip 'Not implemented'
+    end
+  end
+end
